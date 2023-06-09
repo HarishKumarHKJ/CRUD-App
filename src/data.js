7fe5ba3eb0eb9eac7c23c9d3ce7c94c2c1a9bdf6
Harish Kumar <127541964+HarishKumarHKJ@users.noreply.github.com>
2023-06-09 16:35:14 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+        id: 1,
+        name: 'Aravind',
+        username: 'Aravi'
+    },
+    {
+        id: 2,
+        name: 'Shiva',
+        username: 'Shiva The Boss'
+    }
+];
+
+export default userList;
